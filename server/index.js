6fdbf8b6ab131e99c23e9df4c0113f754797e74e
Mui <sweetrain05@hotmail.com>
2023-06-12 21:48:13 -0700

    CRUD applied to category routes

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import morgan from "morgan";
 import authRoutes from "./routes/auth.js"; // node에서는 js를 붙인 full file name을 적어야 한다.
+import categoryRoutes from "./routes/category.js";
 
 // const express = require("express");  // express를 import해오면 필요 없음
 
@@ -22,6 +23,7 @@ app.use(express.json());
 
 // router middleware
 app.use("/api", authRoutes);
+app.use("/api", categoryRoutes);
 
 // process.env에서 PORT가 있으면 그것을 가져오고, 없으면 8000 소환.
 const port = process.env.PORT || 8000;
---
+++ server/routes/category.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const router = express.Router();
+
+// middlewares
+import { isAdmin, requireSignin } from "../middleswares/auth.js";
+
+// controllers
+import { create, update, remove, list, read } from "../controllers/category.js";
+
+router.post("/category", requireSignin, isAdmin, create);
+router.put("/category/:categoryId", requireSignin, isAdmin, update);
+router.delete("/category/:categoryId", requireSignin, isAdmin, remove);
+router.get("/categories", list);
+router.get("/category/:slug", read);
+
+export default router;
