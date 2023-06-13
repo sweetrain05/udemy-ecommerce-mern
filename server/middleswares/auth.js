c59402046ca8db82186e760bcc1b6b0852f22150
Mui <sweetrain05@hotmail.com>
2023-06-12 17:16:08 -0700

    admin authorization applied

--- server/middleswares/auth.js
+++ server/middleswares/auth.js
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken";
+import User from "../models/user.js";
 
 export const requireSignin = (req, res, next) => {
     try {
@@ -6,11 +7,23 @@ export const requireSignin = (req, res, next) => {
             req.headers.authorization,
             process.env.JWT_SECRET
         );
-        console.log("decoded => ", decoded);
         req.user = decoded;
     } catch (err) {
         // 401 error status는 unauthorized를 나타냄
         return res.status(401).json(err);
     }
     next();
+};
+
+export const isAdmin = async (req, res, next) => {
+    try {
+        const user = await User.findById(req.user._id);
+        if (user.role !== 1) {
+            return res.status(401).send("Unauthorized");
+        } else {
+            next();
+        }
+    } catch (err) {
+        console.log(err);
+    }
 };
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,16 +3,14 @@ import express from "express";
 const router = express.Router();
 
 // controllers
-import { register, login } from "../controllers/auth.js";
+import { register, login, secret } from "../controllers/auth.js";
 router.post("/register", register);
 router.post("/login", login);
 
 //middlewares
-import { requireSignin } from "../middleswares/auth.js";
+import { requireSignin, isAdmin } from "../middleswares/auth.js";
 
 // testing
-router.get("/secret", requireSignin, (req, res) => {
-    res.json({ AuthorizedUser: req.user });
-});
+router.get("/secret", requireSignin, isAdmin, secret);
 
 export default router;
