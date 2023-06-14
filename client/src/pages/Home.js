7f36225ce90dad1e6567b0672507ac31ab419931
Mui <sweetrain05@hotmail.com>
2023-06-14 12:33:13 -0700

    client side primary setting with React.js

---
+++ client/src/pages/Home.js
@@ -0,0 +1,10 @@
+import Jumbotron from "../components/cards/Jumbotron";
+
+export default function Home() {
+    return (
+        <div>
+            <Jumbotron />
+            <h1>This is Home page</h1>
+        </div>
+    );
+}
