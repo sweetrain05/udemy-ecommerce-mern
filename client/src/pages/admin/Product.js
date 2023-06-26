aad759daf63b2d85c118a722ef90b51a888dc70b
Mui <sweetrain05@hotmail.com>
2023-06-26 12:30:49 -0700

    Product Create Function Updated

--- client/src/pages/admin/Product.js
+++ client/src/pages/admin/Product.js
@@ -1,10 +1,67 @@
+import { useState, useEffect } from "react";
 import { useAuth } from "../../context/auth";
 import Jumbotron from "../../components/cards/Jumbotron";
 import AdminMenu from "../../components/nav/AdminMenu";
+import axios from "axios";
+import { Select } from "antd";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+
+const { Option } = Select;
 
 export default function AdminProduct() {
     // context
     const [auth, setAuth] = useAuth();
+    // state
+    const [categories, setCategories] = useState([]);
+    const [photo, setPhoto] = useState("");
+    const [name, setName] = useState("");
+    const [description, setDescription] = useState("");
+    const [price, setPrice] = useState("");
+    const [category, setCategory] = useState("");
+    const [shipping, setShipping] = useState("");
+    const [quantity, setQuantity] = useState("");
+    // hook
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        loadCategories();
+    }, []);
+
+    const loadCategories = async () => {
+        try {
+            const { data } = await axios.get("/categories");
+            setCategories(data);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const productData = new FormData();
+            productData.append("photo", photo);
+            productData.append("name", name);
+            productData.append("description", description);
+            productData.append("price", price);
+            productData.append("category", category);
+            productData.append("quantity", quantity);
+            productData.append("shipping", shipping);
+
+            const { data } = await axios.post("/product", productData);
+            if (data?.error) {
+                toast.error(data.error);
+            } else {
+                loadCategories();
+                toast.success(`"${data.name}" is created.`);
+                navigate("/dashboard/admin/products");
+            }
+        } catch (err) {
+            console.log(err);
+            toast.error("Create product failed. Try again.");
+        }
+    };
 
     return (
         <>
@@ -20,9 +77,105 @@ export default function AdminProduct() {
                     </div>
                     <div className="col-md-9">
                         <div className="p-3 mt-2 mb-2 h4 bg-light">
-                            Manage Products
+                            Create Products
                         </div>
-                        <p>create product form...</p>
+
+                        {photo && (
+                            <div className="text-center">
+                                <img
+                                    src={URL.createObjectURL(photo)}
+                                    alt="product photo"
+                                    className="img img-responsive"
+                                    height="200px"
+                                />
+                            </div>
+                        )}
+
+                        <div className="pt-2">
+                            <label className="btn btn-outline-secondary col-12 mb-3">
+                                {photo ? photo.name : "Upload photo"}
+                                <input
+                                    type="file"
+                                    name="photo"
+                                    accept="image/*"
+                                    onChange={(e) => {
+                                        setPhoto(e.target.files[0]);
+                                    }}
+                                    hidden
+                                />
+                            </label>
+                        </div>
+
+                        <input
+                            type="text"
+                            className="form-control p-2 mb-3"
+                            placeholder="Write a name"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        />
+
+                        <textarea
+                            type="text"
+                            className="form-control p-2 mb-3"
+                            placeholder="Write a description"
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                        />
+
+                        <input
+                            type="number"
+                            className="form-control p-2 mb-3"
+                            placeholder="Enter price"
+                            value={price}
+                            onChange={(e) => setPrice(e.target.value)}
+                        />
+
+                        <Select
+                            //showSearch
+                            bordered={true}
+                            size="large"
+                            style={{ width: "100%", marginBottom: "12px" }}
+                            placeholder="Choose category"
+                            onChange={(value) => {
+                                setCategory(value);
+                            }}
+                        >
+                            {categories?.map((c) => (
+                                <Option key={c._id} value={c._id}>
+                                    {c.name}
+                                </Option>
+                            ))}
+                        </Select>
+
+                        <Select
+                            showSearch
+                            bordered={true}
+                            size="large"
+                            style={{ width: "100%", marginBottom: "12px" }}
+                            placeholder="Choose shipping"
+                            onChange={(value) => {
+                                setShipping(value);
+                            }}
+                        >
+                            <Option value="0">No</Option>
+                            <Option value="1">Yes</Option>
+                        </Select>
+
+                        <input
+                            type="number"
+                            min="1"
+                            className="form-control p-2 mb-3"
+                            placeholder="Enter quantity"
+                            value={quantity}
+                            onChange={(e) => setQuantity(e.target.value)}
+                        />
+
+                        <button
+                            onClick={handleSubmit}
+                            className="btn btn-primary mb-5"
+                        >
+                            Submit
+                        </button>
                     </div>
                 </div>
             </div>
